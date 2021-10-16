56844ab6e1c3a334a61482896d87e504e9aa6682
Rings Base <ringsbase@gmail.com>
2021-10-16 15:56:50 +0330

    add chngr

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
 
-            //
+            //chanfe add 
             Console.WriteLine("Hello World!");
         }
     }
